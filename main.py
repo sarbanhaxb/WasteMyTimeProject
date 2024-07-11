809e614550b3f7dd4a88d00fcb63150c73da9247
sarban <sarban7@mail.ru>
2024-07-11 22:48:39 +0300

    Изменения: Переделал просмотр БДО; Сделал фильтры в просмотре БДО;
    Проблемы:
    1. Когда в окне информации нажимаю кнопку сохранить, то фокус с позиции TreeWidget теряется и кнопка return перестает функционировать;
    2. Нужно поработать над фильтрами, чтобы они работали совместно (сложно);

--- main.py
+++ main.py
@@ -112,17 +112,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.refreshEditText()
 
     def updateData(self) -> None:
-        if ref(self.treeWidget.selectedItems()[0], 0) == 0:
-            title = self.CityTitleField.toPlainText()
-            id = self.CityIDField.toPlainText()
-            self.data.updateCityData(title, id)
-            self.PrintTree()
-        elif ref(self.treeWidget.selectedItems()[0], 0) == 1:
-            # тут нужно сделать проверку не поменял ли пользователь название объекта на такое, которое уже есть в таблице objects, иначе крашится программа, так как в таблице objects title уникальное.
-            id = self.ObjectIDField.toPlainText()
-            title = self.ObjectTitleField.toPlainText()
-            self.data.updateObjectData(title, id)
-            self.PrintTree()
+        try:
+            if ref(self.treeWidget.selectedItems()[0], 0) == 0:
+                title = self.CityTitleField.toPlainText()
+                id = self.CityIDField.toPlainText()
+                self.data.updateCityData(title, id)
+                self.PrintTree()
+            elif ref(self.treeWidget.selectedItems()[0], 0) == 1:
+                # тут нужно сделать проверку не поменял ли пользователь название объекта на такое, которое уже есть в таблице objects, иначе крашится программа, так как в таблице objects title уникальное.
+                id = self.ObjectIDField.toPlainText()
+                title = self.ObjectTitleField.toPlainText()
+                self.data.updateObjectData(title, id)
+                self.PrintTree()
+        except IndexError:
+            pass
 
     #Добавляет новый город
     def addNewCity(self) -> None:
@@ -294,6 +297,8 @@ class TableBDO(QDialog):
         self.table_widget.setColumnCount(col)
         self.table_widget.setHorizontalHeaderLabels(columnTitle)
         self.table_widget.setRowCount(row)
+        self.table_widget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+
 
         for i in range(row):
             for j in range(col):
@@ -307,6 +312,7 @@ class TableBDO(QDialog):
                 item.setFlags(item.flags() | 0x0004)
                 self.table_widget.setItem(i, j, item)
             # self.table_widget.verticalHeader().setSectionResizeMode(i, QHeaderView.ResizeToContents)
+
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.table_widget)
@@ -366,50 +372,50 @@ class TableBDO(QDialog):
             else:
                 self.table_widget.setRowHidden(i, True)
 
-class TableBDO1(QTableWidget):
-    def __init__(self, row_count, col_count):
-        super().__init__(row_count, col_count)
-        self.setWindowTitle("Банк данных об отходах")
-        self.setGeometry(0, 0, 1800, 600)
-        self.setWindowModality(2)
-        self.data = SQL.DataBase(PATH)
-        self.bdo = self.data.getBDO()
-        self.setWordWrap(True)
-        header = self.horizontalHeader()
-        for i in range(14):
-            header.setSectionResizeMode(i, QHeaderView.Stretch)
-
-        columnTitle = ("Код по ФККО",
-                       "Наименование вида отхода",
-                       "Происхождение (Производство)",
-                       "Происхождение (Исходная продукция (товар)",
-                       "Происхождение (Процесс)",
-                       "Состав (Наименование компонентов)",
-                       "Состав (Содержание, % масс. (минимум)",
-                       "Состав (Содержание, % масс. (максимум)",
-                       "Примечание о компонентном составе",
-                       "Примечание к виду отхода",
-                       "Агрегатное состояние и физическая форма",
-                       "Класс опасности",
-                       "Критерии отнесения",
-                       "Документ (основание)")
-
-        self.setHorizontalHeaderLabels(columnTitle)
-
-        for i in range(row_count):
-            for j in range(col_count):
-                if str(self.bdo[i][j]) == 'nan':
-                    el = ''
-                else:
-                    el = str(self.bdo[i][j]).strip('\n')
-                item = QTableWidgetItem(el)
-                item.setTextAlignment(0x0001)  # Выравнивание по ширине столбца
-                item.setFlags(item.flags() | 0x0002)  # Флаг на перенос текста по словам
-                item.setFlags(item.flags() | 0x0004)  # Флаг на перенос текста на новую строку
-                self.setItem(i, j, item)
-            self.verticalHeader().setSectionResizeMode(i, QHeaderView.ResizeToContents)
-        self.resizeColumnsToContents()
-        self.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+# class TableBDO1(QTableWidget):
+#     def __init__(self, row_count, col_count):
+#         super().__init__(row_count, col_count)
+#         self.setWindowTitle("Банк данных об отходах")
+#         self.setGeometry(0, 0, 1800, 600)
+#         self.setWindowModality(2)
+#         self.data = SQL.DataBase(PATH)
+#         self.bdo = self.data.getBDO()
+#         self.setWordWrap(True)
+#         header = self.horizontalHeader()
+#         for i in range(14):
+#             header.setSectionResizeMode(i, QHeaderView.Stretch)
+#
+#         columnTitle = ("Код по ФККО",
+#                        "Наименование вида отхода",
+#                        "Происхождение (Производство)",
+#                        "Происхождение (Исходная продукция (товар)",
+#                        "Происхождение (Процесс)",
+#                        "Состав (Наименование компонентов)",
+#                        "Состав (Содержание, % масс. (минимум)",
+#                        "Состав (Содержание, % масс. (максимум)",
+#                        "Примечание о компонентном составе",
+#                        "Примечание к виду отхода",
+#                        "Агрегатное состояние и физическая форма",
+#                        "Класс опасности",
+#                        "Критерии отнесения",
+#                        "Документ (основание)")
+#
+#         self.setHorizontalHeaderLabels(columnTitle)
+#
+#         for i in range(row_count):
+#             for j in range(col_count):
+#                 if str(self.bdo[i][j]) == 'nan':
+#                     el = ''
+#                 else:
+#                     el = str(self.bdo[i][j]).strip('\n')
+#                 item = QTableWidgetItem(el)
+#                 item.setTextAlignment(0x0001)  # Выравнивание по ширине столбца
+#                 item.setFlags(item.flags() | 0x0002)  # Флаг на перенос текста по словам
+#                 item.setFlags(item.flags() | 0x0004)  # Флаг на перенос текста на новую строку
+#                 self.setItem(i, j, item)
+#             self.verticalHeader().setSectionResizeMode(i, QHeaderView.ResizeToContents)
+#         self.resizeColumnsToContents()
+#         self.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
 
 
 if __name__ == "__main__":
